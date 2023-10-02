2c00c9e7c512f449fc786a30432dd2a12f7c78ff
Elisha Okon <eliscodavid@gmail.com>
2023-10-02 12:21:02 +0100

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,11 @@
 /* eslint-disable import/no-default-export */
 import type { NextPage } from 'next';
 import dynamic from 'next/dynamic';
-import LoadingIndicator from '../components/LoadingIndicator';
 
-export const LiFiWidgetNext = dynamic(
+const LiFiWidgetNext = dynamic(
   () => import('../components/Widget').then((module) => module.Widget) as any,
   {
     ssr: false,
-    loading: () => <LoadingIndicator />,
   },
 );
 
